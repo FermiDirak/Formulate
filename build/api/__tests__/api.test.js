15e172df4259a792019569facda169c76f2c7f78
Bryan Manuele <bryan@flexport.com>
2019-01-05 22:35:04 -0800

    set up typescript config

---
+++ build/api/__tests__/api.test.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const formNode_1 = require("../../datastructures/formNode");
+const useLink_1 = __importDefault(require("../useLink"));
+describe('useLink', () => {
+    it('provides the link content', () => {
+        const initialFormData = 'I am a form';
+        const link = formNode_1.createFormNode(initialFormData);
+        const linkContent = useLink_1.default(link);
+        const mockLinkContent = {
+            value: 'I am a form',
+            error: null,
+        };
+        expect(linkContent).toMatchObject(mockLinkContent);
+    });
+});
---
+++ build/api/useLink.js
@@ -0,0 +1,35 @@
+"use strict";
+/**
+ * @description
+ * useLink is used by data entry components to explode a link
+ * down to its respective components
+ *
+ * @example
+ * const TextInput = (link) => {
+ *   {value, onChange, onBlur, error} = useLink(link);
+ *
+ *   return (
+ *     <Container>
+ *       <input type="text" value={value} onChange={onChange} onBlur={onBlur} />
+ *       <p>{error}</p>
+ *     </Container>
+ *   );
+ * }
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const link_1 = require("../datastructures/link");
+/** retrieves the link content associated with a given formNode
+ * @param formNode The FormNode to retrieve the link from
+ * @param validation The validation function to be used on the value
+ * @return The content of the link */
+const useLink = (formNode, validation) => {
+    const linkContent = formNode[link_1.linkSymbol];
+    linkContent.updateErrors(validation);
+    return {
+        value: linkContent.valueRef.getValue(),
+        errors: linkContent.errors.getValue(),
+        onChange: (newValue) => { linkContent.onChange(newValue); },
+        onBlur: linkContent.onBlur,
+    };
+};
+exports.default = useLink;
