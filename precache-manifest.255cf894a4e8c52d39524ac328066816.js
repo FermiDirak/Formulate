9d61561ab7de1e698e2c9c3cf92be43c2474d757
Bryan Manuele <bryan@flexport.com>
2019-01-26 18:34:20 -0800

    Updates

---
+++ precache-manifest.255cf894a4e8c52d39524ac328066816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee19025a4d2e5aeb8335",
+    "url": "/static/js/main.ee19025a.chunk.js"
+  },
+  {
+    "revision": "7b2d402c088fe1798fc0",
+    "url": "/static/js/1.7b2d402c.chunk.js"
+  },
+  {
+    "revision": "ee19025a4d2e5aeb8335",
+    "url": "/static/css/main.1f2beaff.chunk.css"
+  },
+  {
+    "revision": "556ebe43ee0adbd45de5c77fe7e2ba3d",
+    "url": "/index.html"
+  }
+];
