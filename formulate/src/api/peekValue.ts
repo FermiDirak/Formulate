ee070700cedc200942f459843106ae73a8603124
Bryan Manuele <bryan@flexport.com>
2019-03-24 17:24:00 -0700

    updated peekValue documentation

--- formulate/src/api/peekValue.ts
+++ formulate/src/api/peekValue.ts
@@ -1,8 +1,11 @@
 import { Link } from '../datastructures/Link';
 import { linkSymbol } from '../datastructures/MetaLink';
 
-/** Peek a form node's current value
- * @param formNode The node value to peek */
+/**
+ * Peek a form node's current value
+ * @param formNode The node value to peek
+ * @return The value contained within the node value
+ */
 const peekValue = <T>(formNode: Link<T>): T => {
   const { valueRef } = formNode[linkSymbol];
   return valueRef.getValue();
