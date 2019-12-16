92f5582328e961ace396327e9a8f2b25264edfb6
Bryan Manuele <bryan@flexport.com>
2019-12-15 17:57:04 -0800

    created basic test

--- formulate/tests/basicUsage.test.tsx
+++ formulate/tests/basicUsage.test.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import {mount} from "enzyme";
 import useForm from '../src/index';
 
-function BasicForm({onSubmit}: {onSubmit: (formData: any) => void}) {
+type TextInputProps = React.HTMLProps<HTMLInputElement> & {
+  type?: void,
+  onChange: (text: string) => void,
+}
+
+function TextInput({onChange, ...props}: TextInputProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+  return <input type="text" onChange={handleChange} {...props} />;
+}
+
+type BasicFormProps = {
+  onSubmit: (formData: any) => void,
+}
+
+function BasicForm({onSubmit}: BasicFormProps) {
   const formSchema = {
     name: { initial: '', placeholder: 'Alex' }
   }
@@ -11,7 +27,7 @@ function BasicForm({onSubmit}: {onSubmit: (formData: any) => void}) {
 
   return (
     <form>
-      <input type="text" {...formInputs.name} />
+      <TextInput {...formInputs.name} />
       <button type="button" onClick={() => onSubmit(formData)} />
     </form>
   )
@@ -19,18 +35,35 @@ function BasicForm({onSubmit}: {onSubmit: (formData: any) => void}) {
 
 describe("Basic Usage", () => {
   it("renders a basic form", () => {
-    // let formData = null;
-    // const onSubmit = (newFormData: any) => {
-    //   formData = newFormData;
-    // }
-
     const wrapper = mount(<BasicForm onSubmit={() => {}} />);
+    expect(wrapper.find(BasicForm)).toBeTruthy();
+  });
 
-    // console.log(formData);
+  it("calls onClick with initial formData when no actions are made", () => {
+    const onSubmit = jest.fn()
+    const initialFormData = {
+      name: '',
+    };
 
-    console.log(wrapper.debug());
+    const wrapper = mount(<BasicForm onSubmit={onSubmit} />);
+    wrapper.find("button").simulate('click');
 
-    console.log('yay');
+    expect(onSubmit.mock.calls.length).toBe(1);
+    expect(onSubmit.mock.calls[0][0]).toMatchObject(initialFormData);
+  });
 
+  it("calls onClick with formData representing the state of the form", () => {
+    const onSubmit = jest.fn();
+    const expectedFormData = {
+      name: 'formulate',
+    };
+
+    const wrapper = mount(<BasicForm onSubmit={onSubmit} />);
+    wrapper.find("input").simulate('change', {target: {value: 'formulate'}});
+    wrapper.find("button").simulate('click');
+    console.log(wrapper.find("input").debug());
+
+    expect(onSubmit.mock.calls.length).toBe(1);
+    expect(onSubmit.mock.calls[0][0]).toMatchObject(expectedFormData);
   });
 });
