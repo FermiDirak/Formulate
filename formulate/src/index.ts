9c6a0dc19adb6e4504e94caa544cbd2d6b41fa45
Bryan Manuele <bryan@flexport.com>
2019-12-16 16:37:17 -0800

    big brain refactor

--- formulate/src/index.ts
+++ formulate/src/index.ts
@@ -2,36 +2,7 @@ import * as React from 'react';
 import generateInitialFormData from './generateInitialFormData';
 import computeFormInputs from './computeFormInputs';
 import { Action } from './action';
-
-/** Contains data about a form field */
-export type Field<T> = {
-  initial: T,
-  isRequired?: boolean,
-};
-
-/** Props to be spread into a form input */
-type FieldInputProps<T> = {
-  value: T,
-  onChange: (newValue: T) => void,
-}
-
-/** The data represented in a given field */
-type FieldData<T> = T;
-
-/** A schema to represent a form */
-type FormSchema = {
-  [fieldName: string]: Field<any>,
-}
-
-/** Form props to be passed into form inputs */
-type FormInputProps = {
-  [fieldName: string]: FieldInputProps<any>,
-}
-
-/** The data represented in a given form */
-type FormData = {
-  [fieldName: string]: FieldData<any>,
-}
+import { FormSchema, FormData, FormInputProps } from './types';
 
 export default function useForm(
   formSchema: FormSchema,
