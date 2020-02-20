f438a4bbeb14ccb4a3bcaf7c2a34fe6af726df06
Bryan Manuele <bryan@flexport.com>
2020-02-19 19:14:01 -0800

    added cute logo :)

--- example/src/Footer.jsx
+++ example/src/Footer.jsx
@@ -4,11 +4,15 @@ import * as React from "react";
 import './Footer.css';
 import Button from './Button';
 import Spacer from './Spacer';
+import logo from './test_tube.png';
 
 function Footer() {
   return (
     <div className="footer">
-      <p className="footer-title">Formulate</p>
+      <p className="footer-title">
+        <img src={logo} className="footer-logo" alt="logo" />
+        Formulate
+      </p>
       <div className="footer-buttons">
         <Button label="Home" onClick={() => {}} />
         <Spacer />
